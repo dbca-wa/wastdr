95abab82d19137ab1598ffc30c068b211bdcfebe
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2017-12-09 09:56:22 +0800

    add area and site to parse TNE

--- R/parse_turtle_nest_encounters.R
+++ R/parse_turtle_nest_encounters.R
@@ -35,6 +35,8 @@ parse_turtle_nest_encounters <- function(wastd_api_response){
     . <- "Shut up Wesley"
     wastd_api_response$content %>% {
         tibble::tibble(
+            area = purrr::map_dbl(., c("properties", "area")),
+            site = purrr::map_dbl(., c("properties", "site")),
             datetime = purrr::map_chr(., c("properties", "when")) %>% httpdate_as_gmt08,
             longitude = purrr::map_dbl(., c("properties", "longitude")),
             latitude = purrr::map_dbl(., c("properties", "latitude")),
