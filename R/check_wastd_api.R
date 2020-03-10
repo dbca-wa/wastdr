74e56130a084c39f90b472c48ceb0744d2f43146
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-03-10 13:30:40 +0800

    Adjust skip_test_if_offline

--- R/check_wastd_api.R
+++ R/check_wastd_api.R
@@ -1,15 +1,7 @@
-#' #' Check whether API token is set and WAStD is online, else abort
-#' #' @export
-#' check_wastd_api <- function() {
-#'     res <- wastd_GET("")
-#'     if (res$status_code != 200)
-#'         wastdr_msg_abort("WAStD is offline or authentication is missing.")
-#' }
-
 #' Check whether API token is set and WAStD is online, else skip test
 #' @export
 skip_test_if_offline <- function() {
-  res <- wastd_GET("")
+  res <- wastd_GET("", verbose = FALSE)
   if (res$status_code != 200) {
     testthat::skip("WAStD is offline or authentication is missing.")
   }
