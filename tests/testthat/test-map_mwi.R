0cae4ae9c4a5ea5c71516234720c185cbcb01ca3
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-03-10 14:23:44 +0800

    Fix map_mwi and packaged data

--- tests/testthat/test-map_mwi.R
+++ tests/testthat/test-map_mwi.R
@@ -1,5 +1,11 @@
 test_that("map_mwi returns a leaflet htmlwidget", {
-    data("wastd_data")
-    themap <- map_mwi(wastd_data$mwi, sites = wastd_data$sites)
-    testthat::expect_equal(class(themap), c("leaflet", "htmlwidget"))
+  data("wastd_data")
+  themap <- map_mwi(wastd_data$animals, sites = wastd_data$sites)
+  testthat::expect_equal(class(themap), c("leaflet", "htmlwidget"))
+})
+
+test_that("map_mwi tolerates NULL data", {
+  data("wastd_data")
+  themap <- map_mwi(NULL, sites = wastd_data$sites)
+  testthat::expect_equal(class(themap), c("leaflet", "htmlwidget"))
 })
