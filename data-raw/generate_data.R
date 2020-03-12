dc052849abcf35ec94c724742c3a3385f6d71622
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-03-12 16:37:57 +0800

    Rebuild man

--- data-raw/generate_data.R
+++ data-raw/generate_data.R
@@ -11,7 +11,6 @@ sanitize_names <- . %>%
     ~"Name hidden"
   )
 
-
 # Generate animal_encounters ("observed" by author)
 q <- list(taxon = "Cheloniidae", area_id = 17, observer = 4)
 wastd_ae_raw <- wastd_GET("animal-encounters", query = q, max_records = 10)
@@ -66,6 +65,7 @@ usethis::use_data(odkc_data, compress = "xz", overwrite = TRUE)
 wastd_data <- download_wastd_turtledata(max_records = 10)
 wastd_data$surveys <- wastd_data$surveys %>% sanitize_names()
 wastd_data$animals <- wastd_data$animals %>% sanitize_names()
+wastd_data$turtle_morph <- wastd_data$turtle_morph %>% sanitize_names()
 wastd_data$tracks <- wastd_data$tracks %>% sanitize_names()
 wastd_data$nest_dist <- wastd_data$nest_dist %>% sanitize_names()
 wastd_data$nest_tags <- wastd_data$nest_tags %>% sanitize_names()
@@ -75,3 +75,32 @@ wastd_data$nest_fans <- wastd_data$nest_fans %>% sanitize_names()
 wastd_data$nest_fan_outliers <- wastd_data$nest_fan_outliers %>% sanitize_names()
 wastd_data$nest_lightsources <- wastd_data$nest_lightsources %>% sanitize_names()
 usethis::use_data(wastd_data, compress = "xz", overwrite = TRUE)
+
+# Generate tcl
+get_10 <- . %>% wastdr::wastd_GET(max_records = 10) %>% wastd_parse()
+get_all <- . %>% wastdr::wastd_GET() %>% wastd_parse()
+
+tsc_data <- list(
+  # Taxonomy
+  taxon = get_10("taxon"),
+  taxon_fast = get_10("taxon-fast"),
+  vernacular = get_10("vernacular"),
+  crossreference = get_10("crossreference"),
+  community = get_10("community"),
+
+  # Conservation listings
+  conservationlist = get_all("conservationlist"),
+  taxon_conservationlisting = get_10("taxon-conservationlisting"),
+  community_conservationlisting = get_10("community-conservationlisting"),
+
+  # Occurrence
+  occ_taxon_areas = get_10("occ-taxon-areas"),
+  occ_taxon_points = get_10("occ-taxon-points"),
+  occ_community_areas = get_10("occ-community-areas"),
+  occ_taxon_points = get_10("occ-community-points"),
+  occ_observation = get_10("occ-observation"),
+
+  # Conservation documents
+  document = get_10("document")
+)
+usethis::use_data(tsc_data, compress = "xz", overwrite = TRUE)
