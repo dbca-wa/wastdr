dcd9f91a4c5538c6e6e8a5de5837673b0c8e1efb
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-03-27 14:30:07 +0800

    Fix test_check_api

--- tests/testthat/test-check_wastd_api.R
+++ tests/testthat/test-check_wastd_api.R
@@ -1,9 +1,13 @@
 test_that("wastd_works returns FALSE if unauthenticated", {
-  expect_false(wastd_works(api_token = "invalid", api_un = "invalid"))
+    expect_false(wastd_works(
+        api_token = "invalid",
+        api_un = "invalid",
+        api_pw = "invalid"
+    ))
 })
 
 test_that("odkc_works returns FALSE if unauthenticated", {
-  expect_false(odkc_works(url = "http://httpstat.us/401"))
+    expect_false(odkc_works(url = "http://httpstat.us/401"))
 })
 
 # usethis::use_r("check_wastd_api")
