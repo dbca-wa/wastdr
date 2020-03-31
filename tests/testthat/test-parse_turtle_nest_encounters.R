6522a123eb2774aded697d11af81dc392e4909c9
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-03-31 13:06:03 +0800

    Fix test parse TNE

--- tests/testthat/test-parse_turtle_nest_encounters.R
+++ tests/testthat/test-parse_turtle_nest_encounters.R
@@ -1,8 +1,7 @@
 test_that("parse_turtle_nest_encounters parses correctly", {
   data(wastd_tne_raw)
   data(wastd_tne)
-  fresh_tracks <- parse_turtle_nest_encounters(wastd_tne_raw) %>%
-    add_nest_labels()
+  fresh_tracks <- parse_turtle_nest_encounters(wastd_tne_raw)
   expect_equal(length(wastd_tne_raw$data), nrow(wastd_tne))
   expect_equal(nrow(fresh_tracks), nrow(wastd_tne))
 
