ad4d77f401cd674a42f605b18f2f0a451505f525
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2020-04-22 12:35:35 +0800

    linestring

---
+++ tests/testthat/test-gj_linestring_to_st_linestring.R
@@ -0,0 +1,12 @@
+test_that("gj_linestring_to_st_linestring produces sfg", {
+    gj_ls <- paste0("-14.803659 128.403426 10.9 5.9;",
+                    "-14.803719 128.40326 1.7 1.9;",
+                    "-14.803756 128.40317 1.9 1.7;",
+                    "-14.80383 128.402983 1.7 1.6;",
+                    "-14.803913 128.402786 1.4 1.6;")
+    x <- gj_linestring_to_st_linestring(gj_ls)
+
+    testthat::expect_equal(class(x), c("XYZM", "LINESTRING", "sfg"))
+})
+
+# usethis::use_r("gj_linestring_to_st_linestring")
