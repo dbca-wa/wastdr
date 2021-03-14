2264a4adb95845446a9021176ff51d20eab5dd14
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-03-14 08:05:23 +0800

    Add test

--- tests/testthat/test-export_wastd_turtledata.R
+++ tests/testthat/test-export_wastd_turtledata.R
@@ -5,3 +5,10 @@ test_that("export_wastd_turtledata works", {
       export_wastd_turtledata(outdir = t, filename = "test")
     testthat::expect_true(length(fs::dir_ls(t)) > 0)
 })
+
+test_that("export_wastd_turtledata requires a wastd_data object", {
+  nonsense <- list()
+  testthat::expect_error(
+    nonsense %>% export_wastd_turtledata()
+  )
+})
