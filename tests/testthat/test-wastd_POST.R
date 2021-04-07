e1ce300a76004e813ccee998e697b615911d8040
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-04-07 18:37:01 +0800

    Update data

--- tests/testthat/test-wastd_POST.R
+++ tests/testthat/test-wastd_POST.R
@@ -1,4 +1,6 @@
 test_that("wastd_POST returns HTTP 502 on non-existing serializers", {
+  testthat::skip_if_not(wastd_works(), message = "WAStD offline or wrong auth")
+
   expect_warning(
     x <- wastd_POST(
       serializer = "doesnotexist", data = list(),
@@ -12,10 +14,12 @@ test_that("wastd_POST returns HTTP 502 on non-existing serializers", {
     #   api_url = "http://localhost:8220/api/1/", verbose = TRUE)
   )
   expect_equal(class(x), "wastd_api_response")
-  expect_equal(x$status_code, 502)
+  expect_equal(x$status_code, 404)
 })
 
 test_that("wastd_POST errors on authentication failure", {
+  testthat::skip_if_not(wastd_works(), message = "WAStD offline or wrong auth")
+
   expect_warning(
     wastd_POST(
       serializer = "",
