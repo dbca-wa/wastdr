2de995b3d3da1146fc1d1b3fb52bb4444b9101c4
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-04-09 13:26:21 +0800

    GHA: manual codecov and pkgdown works

--- tic.R
+++ tic.R
@@ -1,11 +1,11 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(error_on="error")
 
-if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
-  # creates pkgdown site and pushes to gh-pages branch
-  # only for the runner with the "BUILD_PKGDOWN" env var set
-  do_pkgdown()
-}
+# if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+#   # creates pkgdown site and pushes to gh-pages branch
+#   # only for the runner with the "BUILD_PKGDOWN" env var set
+#   # do_pkgdown()
+# }
 
 get_stage("install") %>%
     # add_step(step_install_github(c("tidyverse/readr"))) %>%
