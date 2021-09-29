06719d3add3c525a8e31428a2a4a078264f1dc25
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-09-30 07:46:38 +0800

    GHA: disable source install overrides

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@ do_package_checks(
     error_on="error",
     args = c(
         "--no-manual",
-        "--as-cran",
+        # "--as-cran",
         "--no-vignettes",
         "--no-build-vignettes",
         "--no-multiarch"
@@ -16,19 +16,19 @@ do_package_checks(
 #   # do_pkgdown()
 # }
 
-get_stage("install") %>%
+# get_stage("install") %>%
     # add_step(step_install_github(c("tidyverse/readr"))) %>%
     # https://stackoverflow.com/q/61875754/2813717 - install proj4
-    add_step(step_install_cran("proj4")) %>%
+    # add_step(step_install_cran("proj4")) %>%
     # sf install fixed by cpp11
-    add_step(step_install_github("r-hub/sysreqs", dependencies = TRUE)) %>%
-    add_step(step_install_github("r-lib/cpp11", dependencies = TRUE)) %>%
-    add_step(step_install_github("r-spatial/sf", dependencies = TRUE)) %>%
-    add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
-    add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
-    add_step(step_install_github("ropensci/geojsonio", dependencies = TRUE)) %>%
-    add_step(step_install_github("Ather-Energy/ggTimeSeries", dependencies = TRUE)) %>%
+    # add_step(step_install_github("r-hub/sysreqs", dependencies = TRUE)) %>%
+    # add_step(step_install_github("r-lib/cpp11", dependencies = TRUE)) %>%
+    # add_step(step_install_github("r-spatial/sf", dependencies = TRUE)) %>%
+    # add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
+    # add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
+    # add_step(step_install_github("ropensci/geojsonio", dependencies = TRUE)) %>%
+    # add_step(step_install_github("Ather-Energy/ggTimeSeries", dependencies = TRUE)) %>%
     # add_step(step_install_cran("listviewer")) %>% # already installed
     # add_step(step_install_cran("geojsonlint")) %>% # already installed
     # libicui8n not found: fixed by stringi forced install
-    add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE))
+    # add_step(step_install_github("gagolews/stringi", dependencies = TRUE, force = TRUE))
