975888d1b5dee6b0b00652e664ad85ee4d242ce9
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-09-30 08:41:17 +0800

    GHA: install ggtimeseries for vignettes

--- tic.R
+++ tic.R
@@ -16,7 +16,7 @@ do_package_checks(
 #   # do_pkgdown()
 # }
 
-# get_stage("install") %>%
+get_stage("install") %>%
     # add_step(step_install_github(c("tidyverse/readr"))) %>%
     # https://stackoverflow.com/q/61875754/2813717 - install proj4
     # add_step(step_install_cran("proj4")) %>%
@@ -27,7 +27,7 @@ do_package_checks(
     # add_step(step_install_github("r-spatial/mapview", dependencies = TRUE)) %>%
     # add_step(step_install_github("r-spatial/leafem", dependencies = TRUE)) %>%
     # add_step(step_install_github("ropensci/geojsonio", dependencies = TRUE)) %>%
-    # add_step(step_install_github("Ather-Energy/ggTimeSeries", dependencies = TRUE)) %>%
+    add_step(step_install_github("Ather-Energy/ggTimeSeries", dependencies = TRUE))
     # add_step(step_install_cran("listviewer")) %>% # already installed
     # add_step(step_install_cran("geojsonlint")) %>% # already installed
     # libicui8n not found: fixed by stringi forced install
