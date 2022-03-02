8d0b7fe8783f67908be93c5335fd7da30807ef30
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-03-02 10:53:15 +0800

    Improve download_wastd_sites: add downloaded_on timestamp

--- R/download_wastd_sites.R
+++ R/download_wastd_sites.R
@@ -7,6 +7,8 @@
 download_wastd_sites <- function(api_url = wastdr::get_wastdr_api_url(),
                                  api_token = wastdr::get_wastdr_api_token(),
                                  verbose = wastdr::get_wastdr_verbose()) {
+
+  downloaded_on <- Sys.time()
   areas_sf <- wastdr::wastd_GET("area") %>%
     magrittr::extract2("data") %>%
     geojsonio::as.json() %>%
@@ -29,7 +31,7 @@ download_wastd_sites <- function(api_url = wastdr::get_wastdr_api_url(),
     ) %>%
     sf::st_join(areas)
 
-  list(localities = areas, sites = sites)
+  list(downloaded_on = downloaded_on, localities = areas, sites = sites)
 }
 
 # usethis::use_test("download_wastd_sites")
