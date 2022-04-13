c47b22d18c66fb746ddc3bd243d7cd3df43a8095
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-04-13 12:07:43 +0800

    Format package

--- tests/testthat/test-map_fanangles.R
+++ tests/testthat/test-map_fanangles.R
@@ -9,19 +9,19 @@ test_that("map_fanangles gatechecks for missing data", {
 })
 
 test_that("map_fanangles works with missing data", {
-    data(wastd_data)
-    # This area name does not exist, the resulting data has the correct cols but
-    # no rows
-    x <- wastd_data %>% filter_wastd_turtledata(area_name="blub")
-    testthat::expect_equal(nrow(x$nest_fans), 0)
-    testthat::expect_equal(nrow(x$nest_fan_outliers), 0)
-    testthat::expect_equal(nrow(x$nest_lightsources), 0)
+  data(wastd_data)
+  # This area name does not exist, the resulting data has the correct cols but
+  # no rows
+  x <- wastd_data %>% filter_wastd_turtledata(area_name = "blub")
+  testthat::expect_equal(nrow(x$nest_fans), 0)
+  testthat::expect_equal(nrow(x$nest_fan_outliers), 0)
+  testthat::expect_equal(nrow(x$nest_lightsources), 0)
 
-    # This should not throw any errors
-    themap <- map_fanangles(x)
+  # This should not throw any errors
+  themap <- map_fanangles(x)
 
-    # map_fanangles should still produce a map, even if empty
-    testthat::expect_equal(class(themap), c("leaflet", "htmlwidget"))
+  # map_fanangles should still produce a map, even if empty
+  testthat::expect_equal(class(themap), c("leaflet", "htmlwidget"))
 })
 
 # use_r("map_fanangles")  # nolint
