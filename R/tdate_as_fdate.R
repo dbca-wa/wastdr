38d862d7113a2d4be3fbbb16c1e88cdfd0d997e0
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-05-18 09:17:15 +0800

    update wordlist, format package

--- R/tdate_as_fdate.R
+++ R/tdate_as_fdate.R
@@ -4,16 +4,16 @@
 #' @importFrom lubridate %--%
 #' @export
 #' @family helpers
-tdate_as_fdate <- function(x){
-    x %>%
-  {
-    lubridate::as.duration(
-      as_date(
-        glue::glue("{wastdr::datetime_as_season(.)}-07-01")
-      ) %--% .
-    )
-  } %>%
-  as.numeric("days")
+tdate_as_fdate <- function(x) {
+  x %>%
+    {
+      lubridate::as.duration(
+        as_date(
+          glue::glue("{wastdr::datetime_as_season(.)}-07-01")
+        ) %--% .
+      )
+    } %>%
+    as.numeric("days")
 }
 
 # usethis::use_test("tdate_as_fdate")
